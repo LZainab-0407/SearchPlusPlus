269ec995e92d56bc8ca148be714cba31a549bb0a
lzainab <143958152+lzainab@users.noreply.github.com>
2025-08-21 04:37:55 +0600

    Add file reading support

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include<fstream>
 
 int main() {
-    std::cout << "Hello, Search Engine Project!" << std::endl;
+    std::cout << std::endl;
+
+    std::ifstream inputFile("doc1.txt");
+
+    if (!inputFile) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+    
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        std::cout << line << std::endl;
+    }
+
+    inputFile.close();
+
+    std::cout << std::endl;
+    
     return 0;
 }
